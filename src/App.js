55084c5c22cbc28de75c03e597fec99abaccb20d
JesseGonzalez024 <jessegonzalez024@gmail.com>
2020-04-09 15:13:50 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -5,11 +5,16 @@ import { addItem } from  './actions/items';
 
 class App extends Component {
 
-  handleOnClick() {
-    this.props.store.dispatch(addItem());
+  // handleOnClick() {
+  //   this.props.store.dispatch(addItem());
+  // }
+
+  handleOnClick = event => {
+    this.props.addItem() // Code change: this.props.dispatch.store is no longer being called
   }
 
   render() {
+    // debugger
     return (
       <div className="App">
         <button onClick={(event) => this.handleOnClick(event)}>
@@ -27,4 +32,30 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps)(App);
+// Code change: this new function takes in dispatch as an argument
+// It then returns an object that contains a function as a value!
+// Notice above in handleOnClick() that this function, addItem(),
+// is what is called, NOT the addItem action creator itself.
+
+const mapDispatchToProps = dispatch => {
+  return {
+    addItem: () => {
+      dispatch(addItem())
+    }
+  };
+};
+
+
+// export default connect(mapStateToProps)(App);
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
+
+// Alternatively:
+
+// export default connect(mapStateToProps, { addItem })(App); // Code change: no mapDispatchToProps function required!
+
+
+// Alternatively: 
+// If we wish to be rid of both functions that are passed inside of the CONNECT() function.
+
+// export default connect(state => ({ items: state.items }), { addItem })(App);
